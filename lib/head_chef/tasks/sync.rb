54fa31bc13bd2be2b177223b43924bc17a5dd994
Mark Corwin <mcorwin@mdsol.com>
2013-10-29 17:35:40 -0400

    HeadChef::Sync::sync now creates a chef environment if it does not exist

--- lib/head_chef/tasks/sync.rb
+++ lib/head_chef/tasks/sync.rb
@@ -9,6 +9,11 @@ module HeadChef
       HeadChef.ui.say('Updating Berksfile.lock...', :cyan)
       Berkshelf.ui.mute { berksfile.update() }
 
+      # Check if environment exits, if not create it
+      unless HeadChef.chef_server.environment.find(chef_environment_name)
+        HeadChef.chef_server.environment.create(name: chef_environment_name)
+      end
+
       # Upload cookbooks before applying environment
       HeadChef.ui.say('Uploading cookbooks to chef server...', :cyan)
       berksfile.upload()
@@ -16,7 +21,7 @@ module HeadChef
       # Apply without lock options argument
       HeadChef.ui.say("Applying Berksfile.lock cookbook version to " \
                       "environment #{environment}...", :cyan)
-      berksfile.apply(environment, {})
+      berksfile.apply(chef_environment_name, {})
     end
   end
 end
