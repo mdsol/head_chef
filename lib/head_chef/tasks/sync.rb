c7f22646d5d8682fe8ba42f13d49b1be4f48ef4e
Mark Corwin <mcorwin@mdsol.com>
2013-11-04 13:46:17 -0500

    Change order of checking chef environment during sync to avoid unnecessary processing

--- lib/head_chef/tasks/sync.rb
+++ lib/head_chef/tasks/sync.rb
@@ -1,6 +1,12 @@
 module HeadChef
   class Sync
     def self.sync(branch, environment)
+      # Check if environment exits, if not create it
+      # Perform first, if it fails no need to continue
+      unless HeadChef.chef_server.environment.find(environment)
+        HeadChef.chef_server.environment.create(name: environment)
+      end
+
       # Retrieves berksfile for branch
       berksfile = HeadChef.berksfile(branch)
 
@@ -8,11 +14,6 @@ module HeadChef
       # dependencies
       HeadChef.ui.say('Updating Berksfile.lock...', :cyan)
       Berkshelf.ui.mute { berksfile.update() }
-
-      # Check if environment exits, if not create it
-      unless HeadChef.chef_server.environment.find(environment)
-        HeadChef.chef_server.environment.create(name: environment)
-      end
 
       # Upload cookbooks before applying environment
       HeadChef.ui.say('Uploading cookbooks to chef server...', :cyan)
