00cd273b4044580ff911df6e8a293d446352746c
Mark Corwin <mcorwin@mdsol.com>
2013-11-08 17:56:53 -0500

    Adds content diff functionality to HeadChef::Diff::diff. HeadChef::Sync::sync now calls diff before altering chef server resources, also has --force option to address cookbook content conflicts.

--- lib/head_chef/tasks/diff.rb
+++ lib/head_chef/tasks/diff.rb
@@ -1,8 +1,19 @@
+require 'digest/md5'
+require 'pathname'
+
 module HeadChef
   class Diff
+    @diff_hash = { add: [],
+                   update: [],
+                   remove: [],
+                   revert: [],
+                   conflict: [] }
+
+    class << self
+      attr_accessor :diff_hash
+    end
+
     def self.diff(branch, environment)
-      # Retrieve dependencies from chef server first,
-      # as if environment does not exist ,we can exit
       HeadChef.ui.say("Loading environment #{environment} from chef server...",
                      :cyan)
       chef_environment = HeadChef.chef_server.environment.find(environment)
@@ -10,63 +21,129 @@ module HeadChef
       if chef_environment
         chef_versions = chef_environment.cookbook_versions
       else
-        #@TODO: implement HeadChef errors w/ codes
-        HeadChef.ui.error "Environment #{environment} not found on chef server."
-        exit(1337)
+        HeadChef.ui.error("Environment #{environment} not found on chef server.")
+        Kernel.exit(1337)
       end
+
       # Retrieves berksfile for branch
       berksfile = HeadChef.berksfile(branch)
 
-      # Ensure lockfile is built
-      HeadChef.ui.say('Updating Berksfile.lock...', :cyan)
-      Berkshelf.ui.mute { berksfile.update }
+      # Rebuild Berksfile.lock to ensure latest cookbooks/dependencies
+      lockfile = berksfile.lockfile
+
+      HeadChef.ui.say('Building Berksfile.lock...', :cyan)
+
+      if File.exists?(lockfile.filepath)
+        File.delete(lockfile.filepath)
+
+        # Reinitialize berksfile/lockfile since old lockfile was present
+        berksfile = HeadChef.berksfile(branch)
+        lockfile = berksfile.lockfile
+      end
+
+      Berkshelf.ui.mute { berksfile.install }
 
       HeadChef.ui.say('Calculating diff...', :cyan)
 
       # Retrieve dependencies from lockfile
-      lockfile_versions = berksfile.lockfile.to_hash[:sources]
+      lockfile_versions = lockfile.to_hash[:sources]
 
       # Iterate through Berkshelf::Dependency hash
-      diff = {add: [],
-              update: [],
-              remove: [],
-              revert: []}
-
       lockfile_versions.each do |cookbook_name, cookbook_source|
-        lockfile_version = cookbook_source.locked_version.to_s
-        chef_version = chef_versions[cookbook_name]
 
-        if chef_version
+        lockfile_version = Semantic::Version.new(cookbook_source.locked_version.to_s)
+
+        # If cookbook version exists on chef server,check it against lockfile
+        # version
+        if chef_versions[cookbook_name]
+          chef_version = Semantic::Version.new(chef_versions[cookbook_name])
+
           if lockfile_version > chef_version
-            diff[:update] << { cookbook_name: cookbook_name,
-                              old_version: chef_version,
-                              new_version: lockfile_version }
+            diff_hash[:update] << { cookbook_name: cookbook_name,
+                                    old_version: chef_version,
+                                    new_version: lockfile_version }
           elsif lockfile_version < chef_version
-            diff[:revert] << { cookbook_name: cookbook_name,
-                              old_version: chef_version,
-                              new_version: lockfile_version }
+            diff_hash[:revert] << { cookbook_name: cookbook_name,
+                                    old_version: chef_version,
+                                    new_version: lockfile_version }
+          elsif lockfile_version == chef_version
+            cookbook_hash = self.content_diff(cookbook_name, chef_version, lockfile)
+            diff_hash[:conflict] << cookbook_hash if cookbook_hash
           end
 
           chef_versions.delete(cookbook_name)
         else
-          diff[:add] << { cookbook_name: cookbook_name,
-                         version: lockfile_version }
+          # Cookbook does not exists on chef server, and therefore will be added
+          # to environment
+          diff_hash[:add] << { cookbook_name: cookbook_name,
+                               version: lockfile_version }
         end
       end
 
+      # Cookbooks in chef environment not in lockfile will be removed from 
+      # environment
       chef_versions.each do |cookbook_name, cookbook_version|
-        diff[:remove] << { cookbook_name: cookbook_name,
-                          version: cookbook_version }
+        diff_hash[:remove] << { cookbook_name: cookbook_name,
+                                version: cookbook_version }
       end
 
-      self.pretty_print_diff_hash(diff)
+      diff_hash
     end
 
-    private
+    def self.content_diff(cookbook_name, cookbook_version, lockfile)
+      # Retrieve cookbook from chef server
+      chef_cookbook = HeadChef.chef_server.cookbook.find(cookbook_name, cookbook_version)
+
+      # If cookbook exists on chef server, download it
+      # Else cookbook is present in lockfile, and will be added to environment
+      if chef_cookbook
+        chef_cookbook.download(".head_chef/#{cookbook_name}")
+      else
+        diff_hash[:add] << { cookbook_name: cookbook_name, version: cookbook_version }
+        return nil
+      end
+
+      # Calculate checksums to verify if contents are the same
+      # Process involves comparing each file in cookbook from chef server exists
+      # and has the same content as cookbook in berkshelf store
+      #
+      # @TODO:
+      #   consider chef ignore files?
+      #   use head_chef cookbook cache?
+      #   ignore lock files?
+
+      # Get paths of cookbooks
+      chef_cookbook_path = ".head_chef/#{cookbook_name}"
+
+      berkshelf_cookbook_hash = lockfile.find(cookbook_name).to_hash
+      berkshelf_cookbook_path = "#{Berkshelf.berkshelf_path}/cookbooks/#{cookbook_name}-#{berkshelf_cookbook_hash[:ref] || berkshelf_cookbook_hash[:locked_version]}"
+
+      # Get all files in chef server cookbook
+      cookbook_files = Dir["#{chef_cookbook_path}/**/*"].reject { |fn| File.directory?(fn) }
+
+      # Diff contents of chef server against contents in berkshelf
+      cookbook_files.each do |cookbook_file|
+        common_cookbook_file = Pathname.new(cookbook_file).relative_path_from(Pathname.new(chef_cookbook_path))
+
+        chef_cookbook_file = "#{chef_cookbook_path}/#{common_cookbook_file}"
+        berkshelf_cookbook_file = "#{berkshelf_cookbook_path}/#{common_cookbook_file}"
+
+        chef_cookbook_checksum = Digest::MD5.digest(File.read(chef_cookbook_file))
+        berkshelf_cookbook_checksum = Digest::MD5.digest(File.read(berkshelf_cookbook_file)) if File.exists?(berkshelf_cookbook_file)
+
+        # If file contents differ, inform user of the conflict]
+        unless chef_cookbook_checksum == berkshelf_cookbook_checksum
+          return { cookbook_name: cookbook_name, version: cookbook_version }
+        end
+      end
+
+      nil
+    end
 
     # @TODO: CLEANUP!... This is gross
+    # Can be refactored by introducting cookbook class
     def self.pretty_print_diff_hash(diff_hash)
-      # Print hash in order of Add, Update, Remove, Revert
+      # Print hash in order of Add, Update, Remove, Revert, Conflict
       HeadChef.ui.say("ADD:", :green) unless diff_hash[:add].empty?
       diff_hash[:add].each do |h|
         HeadChef.ui.say("\t#{h[:cookbook_name]}: #{h[:version]}", :green)
@@ -87,6 +164,10 @@ module HeadChef
         HeadChef.ui.say("\t#{h[:cookbook_name]}: #{h[:old_version]} => #{h[:new_version]}", :red)
       end
 
+      HeadChef.ui.say("CONFLICT:", :red) unless diff_hash[:conflict].empty?
+      diff_hash[:conflict].each do |h|
+        HeadChef.ui.say("\t#{h[:cookbook_name]}: #{h[:version]}", :red)
+      end
     end
   end
 end
--- lib/head_chef/tasks/sync.rb
+++ lib/head_chef/tasks/sync.rb
@@ -1,23 +1,37 @@
 module HeadChef
   class Sync
-    def self.sync(branch, environment)
+    def self.sync(branch, environment, force)
       # Check if environment exits, if not create it
       # Perform first, if it fails no need to continue
       unless HeadChef.chef_server.environment.find(environment)
         HeadChef.chef_server.environment.create(name: environment)
       end
 
-      # Retrieves berksfile for branch
+      # Diff now performs all Berkshelf/lockfile dependency operations
+      HeadChef.ui.say("Determing side effects of sync with chef environment "\
+                      "#{environment}...", :cyan)
+      diff_hash = HeadChef.ui.mute { Diff.diff(branch, environment) }
+
+      unless force
+        unless diff_hash[:conflict].empty?
+          HeadChef.ui.error 'The following cookbooks are in conflict:'
+          diff_hash[:conflict].each do |cookbook_hash|
+            HeadChef.ui.error "#{cookbook_hash[:cookbook_name]}: #{cookbook_hash[:version]}"
+          end
+          HeadChef.ui.error 'Use --force to sync environment'
+          Kernel.exit(1337)
+        end
+      end
+
+      # Retrieve berksfile
       berksfile = HeadChef.berksfile(branch)
 
-      # Berksfile#update to ensure Berksfile.lock has latest cookbook
-      # dependencies
-      HeadChef.ui.say('Updating Berksfile.lock...', :cyan)
-      Berkshelf.ui.mute { berksfile.update() }
-
       # Upload cookbooks before applying environment
+      # @TODO:
+      # pass in cookbook diff, only cookbook bumps/rewrites, no removals
+      # force upload only on conflict cookbooks, otherwise just upload
       HeadChef.ui.say('Uploading cookbooks to chef server...', :cyan)
-      berksfile.upload()
+      berksfile.upload({force: force})
 
       # Apply without lock options argument
       HeadChef.ui.say("Applying Berksfile.lock cookbook version to " \
